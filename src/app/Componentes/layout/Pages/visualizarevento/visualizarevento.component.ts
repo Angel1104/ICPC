4e4b69793147820ea2b5d8782641f806d30cc307
VinniMagne <vinni.magne@gmail.com>
2023-10-21 17:23:40 -0400

    sweet alert de eliminar evento

--- src/app/Componentes/layout/Pages/visualizarevento/visualizarevento.component.ts
+++ src/app/Componentes/layout/Pages/visualizarevento/visualizarevento.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-visualizarevento',
@@ -6,20 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./visualizarevento.component.css']
 })
 export class VisualizareventoComponent {
-  eliminarConfirmado = false;
-  mostrarModal = false;
-
-  confirmarEliminacion() {
-    this.mostrarModal = true;
-  }
-
-  confirmacionEliminacion(confirmado: boolean) {
-    this.eliminarConfirmado = confirmado;
-    this.mostrarModal = false;
-
-    if (confirmado) {
-      // Realiza la eliminación del evento y muestra un mensaje de éxito.
-      console.log('Se ha eliminado el evento con éxito');
-    }
+  constructor(private router: Router) {}
+  mostrarSweetAlert() {
+    Swal.fire({
+      title: '¿Estás seguro de eliminar el evento?',
+      showDenyButton: true,
+      confirmButtonText: 'Aceptar',
+      denyButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Aquí puedes colocar el código para eliminar el evento
+        Swal.fire('Se ha eliminado el evento con éxito', '', 'success').then(() => {
+          // Después de eliminar el evento y mostrar el mensaje de éxito, redirige a la página de eventos
+          this.router.navigate(['/admin/evento']);
+        });
+      } else if (result.isDenied) {
+        Swal.fire('Operación cancelada', '', 'error');
+      }
+    });
   }
 }
